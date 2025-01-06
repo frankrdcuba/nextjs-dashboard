cad1475f4393695b41b7c48efeb5d3cbd1593208
Frank Rodriguez Diaz <frankrdcuba@gmail.com>
2025-01-06 11:14:20 -0300

    finish

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-    return <p>Customers Page</p>
+import { fetchCustomersPages } from '@/app/lib/data';
+import Pagination from '@/app/ui/invoices/pagination';
+import { Metadata } from 'next';
+import Table from '@/app/ui/customers/table'
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchCustomersPages(query);
+    return (
+        <div className="w-full">
+            <Table query={query} currentPage={currentPage} />
+            <div className="mt-5 flex w-full justify-center">
+                <Pagination totalPages={totalPages} />
+            </div>
+        </div>
+    );
+
 }
